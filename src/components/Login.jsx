a996460f67f41eb0a777dbaf48a1595171f94d89
Shakib-Shajid <shakibalshajid1682@gmail.com>
2023-12-22 16:33:10 +0600

    authentication added

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -2,9 +2,20 @@ import { useContext, useState } from "react";
 import { Helmet } from "react-helmet-async";
 import { Link, Navigate } from "react-router-dom";
 import { AuthContext } from "../Provider/AuthProvider";
+import { FaGoogle } from 'react-icons/fa';
+import { GoogleAuthProvider, getAuth, signInWithPopup, signOut } from "firebase/auth";
+import app from "../firebase/firebase.config";
+
+
+
 
 const Login = () => {
     const [loginError, setloginError] = useState()
+    const [user, setUser] = useState(null);
+    const auth = getAuth(app);
+    const provider = new GoogleAuthProvider('');
+
+
 
     const { signIn } = useContext(AuthContext);
 
@@ -50,6 +61,40 @@ const Login = () => {
             })
 
     }
+
+    const handleGoogleSignIn = () => {
+        // console.log('google mama is coming')
+        signInWithPopup(auth, provider)
+
+            .then(result => {
+                const loggedInUser = result.user;
+                // console.log(loggedInUser);
+                setUser(loggedInUser);
+                //navigate after login
+                Navigate(location?.state ? location.state : '/')
+            })
+
+
+            .catch(error => {
+                // console.log('error' , error.message)
+                setloginError(error.message);
+
+
+            })
+        //google signout
+        const handleSignOut = () => {
+            signOut(auth)
+                .then(result => {
+                    console.log(result);
+                    setUser(null);
+                })
+
+                .catch(error => {
+                    console.log(error)
+                })
+        }
+
+    }
     return (
         <div>
             <Helmet>
@@ -83,6 +128,10 @@ const Login = () => {
 
                 <p className="text-center mt-4">Dont have an account <Link to="/register" className="text-blue-600 font-bold">Register</Link></p>
             </div>
+            <div className="flex justify-center items-center  md:w-3/4 lg:w-1/2 mx-auto btn btn-success mt-3 text-white">
+                <FaGoogle className=" mx-2"></FaGoogle> <button onClick={handleGoogleSignIn}>Login with Google</button>
+            </div>
+
         </div>
     );
 };
