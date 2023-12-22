865fcab4089bfcfeda41c71a018405d7e64009e4
Shakib-Shajid <shakibalshajid1682@gmail.com>
2023-12-22 15:19:25 +0600

    login and register added

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,12 +1,43 @@
 import { Helmet } from "react-helmet-async";
+import { Link } from "react-router-dom";
 
 const Login = () => {
+    const handleLogin = () => {
+        console.log("login")
+    }
     return (
         <div>
             <Helmet>
                 <title>To Do | Login</title>
             </Helmet>
-            <h3>This is login</h3>
+            <div data-aos="flip-up">
+                <h2 className="text-3xl my-10 text-center">Please Login</h2>
+
+                <form onSubmit={handleLogin} className=" md:w-3/4 lg:w-1/2 mx-auto" >
+
+
+                    <div className="form-control">
+                        <label className="label">
+                            <span className="label-text">Email</span>
+                        </label>
+                        <input type="email" placeholder="email" className="input input-bordered" required name="email" />
+                    </div>
+                    <div className="form-control">
+                        <label className="label">
+                            <span className="label-text">Password</span>
+                        </label>
+                        <input type="password" placeholder="password" className="input input-bordered" required name="password" />
+                        <label className="label">
+                            <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+                        </label>
+                    </div>
+                    <div className="form-control mt-6">
+                        <button className="btn btn-primary">Login</button>
+                    </div>
+                </form>
+
+                <p className="text-center mt-4">Dont have an account <Link to="/register" className="text-blue-600 font-bold">Register</Link></p>
+            </div>
         </div>
     );
 };
